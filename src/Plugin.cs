88c2cb86b87df8a3892a1bd66de768f52127aa5e
ToxesFoxes <toxes_foxes@outlook.com>
2026-06-06 20:06:09 +0500

    chore: Bump version to 1.1.2 in manifest files

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -9,7 +9,7 @@ using RepoSteamNetworking.API;
 
 namespace TFS_Mimics
 {
-    [BepInPlugin("TFS_Mimics", "TFS_Mimics", "1.1.1")]
+    [BepInPlugin("TFS_Mimics", "TFS_Mimics", "1.1.2")]
     [BepInDependency("com.rune580.reposteamnetworking")]
     public class Plugin : BaseUnityPlugin
     {
